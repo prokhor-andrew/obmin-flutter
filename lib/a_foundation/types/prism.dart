bd3d67de9031457986b976f68409611904704e89
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-05-21 16:30:43 -0500

    Prism put

--- lib/a_foundation/types/prism.dart
+++ lib/a_foundation/types/prism.dart
@@ -2,10 +2,10 @@ import 'package:obmin_concept/a_foundation/types/optional.dart';
 
 final class Prism<Whole, Part> {
   final Optional<Part> Function(Whole whole) get;
-  final Whole Function(Whole whole, Part part) set;
+  final Whole Function(Whole whole, Part part) put;
 
   Prism({
     required this.get,
-    required this.set,
+    required this.put,
   });
 }
