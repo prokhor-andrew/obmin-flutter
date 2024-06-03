9c1ac58260cd24e56d2cd383ec72cbc55fa73b9b
prokhor-andrew <prokhor.andrew@gmail.com>
2024-06-02 20:43:49 -0500

    Added map method to Either

--- lib/a_foundation/types/either.dart
+++ lib/a_foundation/types/either.dart
@@ -24,10 +24,7 @@ sealed class Either<L, R> {
   Either<L, RightResult> mapRightTo<RightResult>(RightResult value) => mapRight((_) => value);
 
   Either<L, C> map<C>(C Function(R value) function) {
-    return switch (this) {
-      Left<L, R>(value: final value) => Left(value),
-      Right<L, R>(value: final value) => Right(function(value)),
-    };
+    return mapRight(function);
   }
 
   Either<L, C> bind<C>(Either<L, C> Function(R value) function) {
