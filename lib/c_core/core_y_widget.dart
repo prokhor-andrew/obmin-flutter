13fe51272af6322b5065f4ce7bc0e556cf3e2ee8
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-06-03 13:38:59 -0500

    Trying to fix type casting issues

--- lib/c_core/core_y_widget.dart
+++ lib/c_core/core_y_widget.dart
@@ -12,8 +12,16 @@ CoreWidget<(DomainState, void Function(Writer<DomainState, Loggable> Function(Do
   required Core<DomainState, DomainState, Writer<DomainState, Loggable> Function(DomainState state), Loggable> core,
   required Widget Function(BuildContext context, DomainState state, void Function(Writer<DomainState, Loggable> Function(DomainState state) update)?) builder,
 }) {
-  return CoreWidget<(DomainState, void Function(Writer<DomainState, Loggable> Function(DomainState))?), DomainState, DomainState,
-      Writer<DomainState, Loggable> Function(DomainState), Loggable>(
+  return CoreWidget<
+      (
+      DomainState,
+      void Function(Writer<DomainState, Loggable> Function(DomainState state) reducer)? update
+      ),
+      DomainState,
+      DomainState,
+      Writer<DomainState, Loggable> Function(DomainState state),
+      Loggable
+  >(
     core: core,
     init: (state) {
       return (state, null);
