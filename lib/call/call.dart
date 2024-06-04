7d1f8b103b2657363e26a2c3dcbc57ff7caf7521
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-06-04 10:22:30 -0500

    Removed Failure state for Call

--- lib/call/call.dart
+++ lib/call/call.dart
@@ -2,22 +2,22 @@
 // This file is part of Obmin, licensed under the MIT License.
 // See the LICENSE file in the project root for license information.
 
-sealed class Call<Req, Err, Res> {}
+sealed class Call<Req, Res> {}
 
-final class Launched<Req, Err, Res> extends Call<Req, Err, Res> {
+final class Launched<Req, Res> extends Call<Req, Res> {
   final Req req;
 
   Launched(this.req);
 
   @override
   String toString() {
-    return "Launched<$Req, $Err, $Res> { req=$req }";
+    return "Launched<$Req, $Res> { req=$req }";
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    if (other is! Launched<Req, Err, Res>) return false;
+    if (other is! Launched<Req, Res>) return false;
     return req == other.req;
   }
 
@@ -25,41 +25,20 @@ final class Launched<Req, Err, Res> extends Call<Req, Err, Res> {
   int get hashCode => req.hashCode;
 }
 
-final class Failure<Req, Err, Res> extends Call<Req, Err, Res> {
-  final Err err;
-
-  Failure(this.err);
-
-  @override
-  String toString() {
-    return "Failure<$Req, $Err, $Res> { err=$err }";
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    if (other is! Failure<Req, Err, Res>) return false;
-    return err == other.err;
-  }
-
-  @override
-  int get hashCode => err.hashCode;
-}
-
-final class Success<Req, Err, Res> extends Call<Req, Err, Res> {
+final class Success<Req, Res> extends Call<Req, Res> {
   final Res res;
 
   Success(this.res);
 
   @override
   String toString() {
-    return "Success<$Req, $Err, $Res> { res=$res }";
+    return "Success<$Req,  $Res> { res=$res }";
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    if (other is! Success<Req, Err, Res>) return false;
+    if (other is! Success<Req, Res>) return false;
     return res == other.res;
   }
 
