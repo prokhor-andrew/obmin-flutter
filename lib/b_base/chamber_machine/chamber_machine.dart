ede2cae06374d5291f91db6d570bc55d0b698404
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-06-24 11:11:16 -0500

    Fixed naming for ChamberMachine

--- lib/b_base/chamber_machine/chamber_machine.dart
+++ lib/b_base/chamber_machine/chamber_machine.dart
@@ -5,6 +5,7 @@
 import 'package:obmin/a_foundation/channel/channel.dart';
 import 'package:obmin/a_foundation/machine.dart';
 import 'package:obmin/a_foundation/machine_factory.dart';
+import 'package:obmin/b_base/chamber_machine/silo_machine.dart';
 import 'package:obmin/b_base/feature_machine/feature.dart';
 import 'package:obmin/b_base/feature_machine/feature_machine.dart';
 
@@ -13,13 +14,13 @@ extension ChamberMachine on MachineFactory {
     required String id,
     required Future<Helper> Function() onCreateHelper,
     required Future<void> Function(Helper helper) onDestroyHelper,
-    required Set<Machine<(), Output>> Function(Helper helper) initial,
-    required Set<Machine<(), Output>> Function(Helper helper, Input input) map,
+    required Set<Silo<Output>> Function(Helper helper) initial,
+    required Set<Silo<Output>> Function(Helper helper, Input input) map,
     ChannelBufferStrategy<Input>? inputBufferStrategy,
     ChannelBufferStrategy<Output>? outputBufferStrategy,
     ChannelBufferStrategy<FeatureEvent<Output, Input>>? internalBufferStrategy,
   }) {
-    Feature<Helper, Output, (), Input, Output> config(Helper helper, Set<Machine<(), Output>> machines) {
+    Feature<Helper, Output, (), Input, Output> config(Helper helper, Set<Silo<Output>> machines) {
       return Feature.create(
         state: helper,
         machines: machines,
@@ -58,7 +59,7 @@ extension ChamberMachine on MachineFactory {
     required Future<Helper> Function() onCreateHelper,
     required Future<void> Function(Helper helper) onDestroyHelper,
     required State initial,
-    required Set<Machine<(), State Function(State)>> Function(Helper helper, State state) map,
+    required Set<Silo<State Function(State)>> Function(Helper helper, State state) map,
     ChannelBufferStrategy<State>? inputBufferStrategy,
     ChannelBufferStrategy<State Function(State)>? outputBufferStrategy,
     ChannelBufferStrategy<FeatureEvent<State Function(State), State>>? internalBufferStrategy,
