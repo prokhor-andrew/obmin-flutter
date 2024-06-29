8162c9ee6d9585d80ee7c54ccf9f836b621ce951
prokhor-andrew <prokhor.andrew@gmail.com>
2024-06-29 18:49:01 -0500

    separated swap args extension from currying

--- lib/utils/swap_args.dart
+++ lib/utils/swap_args.dart
@@ -2,12 +2,11 @@
 // This file is part of Obmin, licensed under the MIT License.
 // See the LICENSE file in the project root for license information.
 
-extension CurryingExtension<A, B, R> on R Function(A, B) {
-  R Function(B) Function(A) curry() {
-    return (A a) {
-      return (B b) {
-        return this(a, b);
-      };
+extension SwapArgsExtension<A, B, R> on R Function(A, B) {
+
+  R Function(B, A) swapArgs() {
+    return (B b, A a) {
+      return this(a, b);
     };
   }
 }
