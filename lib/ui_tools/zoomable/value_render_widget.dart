ce83995245ae7fc85292d7f32e4088c1a55fec79
prokhor-andrew <prokhor.andrew@gmail.com>
2024-06-30 13:35:11 -0500

    Made ValueRenderWidget into typedef

--- lib/ui_tools/zoomable/value_render_widget.dart
+++ lib/ui_tools/zoomable/value_render_widget.dart
@@ -7,27 +7,12 @@ import 'package:obmin/ui_tools/zoomable/render_widget.dart';
 import 'package:obmin/ui_tools/zoomable/zoomable_widget.dart';
 
 extension ValueRenderWidgetExtension<T> on Zoomable<T, T Function(T)> {
- 
   Widget valueRender({
     Key? key,
     required Widget Function(BuildContext context, T state, void Function(T Function(T value) transition) update) builder,
   }) {
-    return ValueRenderWidget(this, key: key, builder: builder);
+    return ValueRenderWidget<T>(this, key: key, builder: builder);
   }
 }
 
-class ValueRenderWidget<T> extends StatelessWidget {
-  final Zoomable<T, T Function(T)> zoomable;
-  final Widget Function(BuildContext context, T state, void Function(T Function(T) transition) update) builder;
-
-  const ValueRenderWidget(
-    this.zoomable, {
-    super.key,
-    required this.builder,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return zoomable.render(key: key, builder: builder);
-  }
-}
+typedef ValueRenderWidget<T> = RenderWidget<T, T Function(T)>;
