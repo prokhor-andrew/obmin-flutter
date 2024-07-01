4d3625f84bc072df0a78818df8c75d2009af224c
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-07-01 11:35:05 -0500

    Created optics for call and result

---
+++ lib/call/optics/call_optics.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2024 Andrii Prokhorenko
+// This file is part of Obmin, licensed under the MIT License.
+// See the LICENSE file in the project root for license information.
+
+import 'package:obmin/call/call.dart';
+import 'package:obmin/optics/iso.dart';
+import 'package:obmin/optics/optics_factory.dart';
+import 'package:obmin/optics/prism.dart';
+import 'package:obmin/optics_ext/either_optics.dart';
+import 'package:obmin/types/either.dart';
+
+extension CallOptics on OpticsFactory {
+  Iso<Call<Req, Res>, Either<Req, Res>> callToEitherIso<Req, Res>() {
+    return Iso(
+      to: (value) {
+        return value.asEither();
+      },
+      from: (value) {
+        return value.asCall();
+      },
+    );
+  }
+
+  Iso<Either<Req, Res>, Call<Req, Res>> eitherToCallIso<Req, Res>() {
+    return Iso(
+      to: (value) {
+        return value.asCall();
+      },
+      from: (value) {
+        return value.asEither();
+      },
+    );
+  }
+
+  Prism<Call<Req, Res>, Req> callToReqPrism<Req, Res>() {
+    return callToEitherIso<Req, Res>().asPrism().then(eitherToLeftPrism<Req, Res>());
+  }
+
+  Prism<Call<Req, Res>, Res> callToResPrism<Req, Res>() {
+    return callToEitherIso<Req, Res>().asPrism().then(eitherToRightPrism<Req, Res>());
+  }
+}
---
+++ lib/call/optics/result_optics.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2024 Andrii Prokhorenko
+// This file is part of Obmin, licensed under the MIT License.
+// See the LICENSE file in the project root for license information.
+
+import 'package:obmin/call/result.dart';
+import 'package:obmin/optics/iso.dart';
+import 'package:obmin/optics/optics_factory.dart';
+import 'package:obmin/optics/prism.dart';
+import 'package:obmin/optics_ext/either_optics.dart';
+import 'package:obmin/types/either.dart';
+
+extension ResultOptics on OpticsFactory {
+  Iso<Result<Res, Err>, Either<Res, Err>> resultToEitherIso<Res, Err>() {
+    return Iso(
+      to: (value) {
+        return value.asEither();
+      },
+      from: (value) {
+        return value.asResult();
+      },
+    );
+  }
+
+  Iso<Either<Res, Err>, Result<Res, Err>> eitherToResultIso<Res, Err>() {
+    return Iso(
+      to: (value) {
+        return value.asResult();
+      },
+      from: (value) {
+        return value.asEither();
+      },
+    );
+  }
+
+  Prism<Result<Res, Err>, Res> resultToResPrism<Res, Err>() {
+    return resultToEitherIso<Res, Err>().asPrism().then(eitherToLeftPrism<Res, Err>());
+  }
+
+  Prism<Result<Res, Err>, Err> resultToErrPrism<Res, Err>() {
+    return resultToEitherIso<Res, Err>().asPrism().then(eitherToRightPrism<Res, Err>());
+  }
+}
