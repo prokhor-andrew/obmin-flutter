c10c170561494e5420288ccb0b5b2ef9d39a233e
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-07-02 14:41:00 -0500

    Created ==, hash and toString for RecursiveCall

--- lib/call/recursive_call.dart
+++ lib/call/recursive_call.dart
@@ -8,4 +8,18 @@ final class RecursiveCall<Req, Res> {
   final Call<Res, Call<Req, RecursiveCall<Req, Res>>> call;
 
   RecursiveCall(this.call);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is RecursiveCall<Req, Res> && other.call == call;
+  }
+
+  @override
+  int get hashCode => call.hashCode;
+
+  @override
+  String toString() {
+    return "RecursiveCall<$Req, $Res> { call=$call }";
+  }
 }
