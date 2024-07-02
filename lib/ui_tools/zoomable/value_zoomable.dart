5d1a1d2b4426b51630ae3eda1375fc413a03fa34
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-07-02 14:05:53 -0500

    Created typedef for ValueZoomable

---
+++ lib/ui_tools/zoomable/value_zoomable.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 Andrii Prokhorenko
+// This file is part of Obmin, licensed under the MIT License.
+// See the LICENSE file in the project root for license information.
+
+import 'package:obmin/types/transition.dart';
+import 'package:obmin/ui_tools/zoomable/zoomable_widget.dart';
+
+typedef ValueZoomable<T> = Zoomable<T, Transition<T>>;
