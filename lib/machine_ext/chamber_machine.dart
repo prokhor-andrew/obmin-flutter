a83320ad998b095ceec0e236bc0bf5cc59f28e88
prokhor-andrew <prokhor.andrew@gmail.com>
2024-07-11 23:42:29 -0500

    Removed call machine and chamberY

--- lib/machine_ext/chamber_machine.dart
+++ lib/machine_ext/chamber_machine.dart
@@ -2,17 +2,12 @@
 // This file is part of Obmin, licensed under the MIT License.
 // See the LICENSE file in the project root for license information.
 
-import 'package:obmin/call/call.dart';
 import 'package:obmin/channel/channel_lib.dart';
 import 'package:obmin/machine/machine.dart';
 import 'package:obmin/machine/machine_factory.dart';
-import 'package:obmin/machine_ext/call_machine.dart';
 import 'package:obmin/machine_ext/feature_machine/feature.dart';
 import 'package:obmin/machine_ext/feature_machine/feature_machine.dart';
 import 'package:obmin/machine_ext/silo_machine.dart';
-import 'package:obmin/optics/affine.dart';
-import 'package:obmin/optics/iso.dart';
-import 'package:obmin/types/optional.dart';
 import 'package:obmin/types/transition.dart';
 
 extension ChamberMachine on MachineFactory {
@@ -82,64 +77,5 @@ extension ChamberMachine on MachineFactory {
       outputBufferStrategy: outputBufferStrategy,
       internalBufferStrategy: internalBufferStrategy,
     );
-  }
-
-  Machine<State, Transition<State>> chamberY<State, Helper>({
-    required String id,
-    required Future<Helper> Function() onCreateHelper,
-    required Future<void> Function(Helper helper) onDestroyHelper,
-    required State initial,
-    required List<ChamberConfig<State>> Function(Helper helper) map,
-    ChannelBufferStrategy<State>? inputBufferStrategy,
-    ChannelBufferStrategy<Transition<State>>? outputBufferStrategy,
-    ChannelBufferStrategy<FeatureEvent<Transition<State>, State>>? internalBufferStrategy,
-  }) {
-    return chamberX<State, Helper>(
-      id: id,
-      onCreateHelper: onCreateHelper,
-      onDestroyHelper: onDestroyHelper,
-      initial: initial,
-      map: (helper, state) {
-        final Set<Silo<Transition<State>>> result = {};
-
-        for (final config in map(helper)) {
-          switch (config._silo(state)) {
-            case None<Silo<Transition<State>>>():
-              break;
-            case Some<Silo<Transition<State>>>(value: final value):
-              result.add(value);
-              break;
-          }
-        }
-
-        return result;
-      },
-    );
-  }
-}
-
-final class ChamberConfig<Whole> {
-  final Optional<Silo<Transition<Whole>>> Function(Whole) _silo;
-
-  ChamberConfig._(this._silo);
-
-  static ChamberConfig<Whole> create<Whole, Req, Res>({
-    required Affine<Whole, Call<Req, Res>> affine,
-    required Silo<Res> Function(Req req) silo,
-  }) {
-    return ChamberConfig._((state) {
-      return MachineFactory.shared.call(
-        state: state,
-        affine: affine,
-        silo: silo,
-      );
-    });
-  }
-
-  static ChamberConfig<Whole> identity<Whole, Req, Res>({
-    required Affine<Whole, Call<Req, Res>> Function(Affine<Whole, Whole> affine) affine,
-    required Silo<Res> Function(Req req) silo,
-  }) {
-    return create(affine: affine(Iso.identity<Whole>().asAffine()), silo: silo);
   }
 }
