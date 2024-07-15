9ad3339e4376b05c034b7ea8361b793a0045098b
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-07-15 16:38:15 -0500

    Brought back mapWithOptic that was mapWithAffine before

---
+++ lib/machine_ext/map_with_optic_machine.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 Andrii Prokhorenko
+// This file is part of Obmin, licensed under the MIT License.
+// See the LICENSE file in the project root for license information.
+
+import 'package:obmin/channel/channel_lib.dart';
+import 'package:obmin/machine_ext/feature_machine/feature.dart';
+import 'package:obmin/machine_ext/map_output_machine.dart';
+import 'package:obmin/machine_ext/silo_machine.dart';
+import 'package:obmin/optics/settable/setter.dart';
+import 'package:obmin/types/update.dart';
+
+extension MapWithOpticMachineExtension<T> on Silo<T> {
+  Silo<Update<R>> mapWithOptic<R>(
+    Setter<R, T> setter, {
+    ChannelBufferStrategy<()>? inputBufferStrategy,
+    ChannelBufferStrategy<Update<R>>? outputBufferStrategy,
+    ChannelBufferStrategy<FeatureEvent<T, ()>>? internalBufferStrategy,
+  }) {
+    return map<Update<R>>(
+      (value) {
+        return (whole) {
+          return setter.set(whole, value);
+        };
+      },
+      internalBufferStrategy: internalBufferStrategy,
+      inputBufferStrategy: inputBufferStrategy,
+      outputBufferStrategy: outputBufferStrategy,
+    );
+  }
+}
