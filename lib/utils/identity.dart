f0821fb7221eff3f99f3015b5ffcda5c65cfb5a7
prokhor-andrew <prokhor.andrew@gmail.com>
2024-07-22 12:18:06 -0500

    Identity function created

---
+++ lib/utils/identity.dart
@@ -0,0 +1,6 @@
+// Copyright (c) 2024 Andrii Prokhorenko
+// This file is part of Obmin, licensed under the MIT License.
+// See the LICENSE file in the project root for license information.
+
+
+T identity<T>(T value) => value;
