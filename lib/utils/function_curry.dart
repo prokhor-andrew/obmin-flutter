0771569cb2f1d57e85d54c8aeaf24570eb4127b9
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-07-29 17:15:15 -0500

    Fixed argsSwapped. Renamed curry in curried. Made bother gettable properties

--- lib/utils/function_curry.dart
+++ lib/utils/function_curry.dart
@@ -2,14 +2,10 @@
 // This file is part of Obmin, licensed under the MIT License.
 // See the LICENSE file in the project root for license information.
 
-
 extension FunctionCurryExtension<T1, T2, R> on R Function(T1, T2) {
-
-  R Function(T2) Function(T1) curry() {
-    return (t1) {
-      return (t2) {
-        return this(t1, t2);
+  R Function(T2) Function(T1) get curried => (t1) {
+        return (t2) {
+          return this(t1, t2);
+        };
       };
-    };
-  }
 }
