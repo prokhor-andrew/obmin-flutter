7aa0f7ff77c1876a584b8599ba27bcd5c7e92bf7
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-07-29 17:02:23 -0500

    Added curry extension for functions

---
+++ lib/utils/function_curry.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 Andrii Prokhorenko
+// This file is part of Obmin, licensed under the MIT License.
+// See the LICENSE file in the project root for license information.
+
+
+extension FunctionCurryExtension<T1, T2, R> on R Function(T1, T2) {
+
+  R Function(T2) Function(T1) curry() {
+    return (t1) {
+      return (t2) {
+        return this(t1, t2);
+      };
+    };
+  }
+}
