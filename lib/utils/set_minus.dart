12c7745c2ae67ba4f5e29c1497a12596c1ec811c
AndriiProkhorenko <prokhor.andrew@gmail.com>
2024-09-06 13:55:23 -0500

    created set_plus.dart, set_minus.dart, list_zip.dart extensions

---
+++ lib/utils/set_minus.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 Andrii Prokhorenko
+// This file is part of Obmin, licensed under the MIT License.
+// See the LICENSE file in the project root for license information.
+
+extension MinusOnSetExtension<T> on Set<T> {
+  Set<T> minusWhere(bool Function(T value) predicate) {
+    final list = toList();
+
+    if (isEmpty) {
+      return this;
+    }
+
+    int searchIndex = -1;
+
+    for (int i = 0; i < length; i++) {
+      if (predicate(list[i])) {
+        searchIndex = i;
+        break;
+      }
+    }
+
+    if (searchIndex == -1) {
+      return this;
+    }
+
+    list.removeAt(searchIndex);
+
+    return list.toSet();
+  }
+}
