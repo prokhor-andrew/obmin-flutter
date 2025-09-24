6f510593d8586710bccc0d5793d0a729914fcd41
andriiprokhorenko <prokhor.andrew@gmail.com>
2025-09-24 11:47:22 -0500

    made optic typedef again. removed affine optic. added list, option, either, these, writer, validator optics

--- lib/optics/poly_optic.dart
+++ lib/optics/poly_optic.dart
@@ -3,25 +3,33 @@
 // See the LICENSE file in the project root for license information.
 
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
-import 'package:obmin/optics/optic.dart';
 import 'package:obmin/types/either.dart';
 import 'package:obmin/types/func.dart';
 import 'package:obmin/types/list.dart';
+import 'package:obmin/types/option.dart';
+import 'package:obmin/types/these.dart';
+import 'package:obmin/types/tuple.dart';
+import 'package:obmin/types/validator.dart';
+import 'package:obmin/types/writer.dart';
 
 final class PolyOptic<Whole, TWhole, Part, TPart> {
   final Func<Func<Part, TPart>, Func<Whole, TWhole>> run;
 
-  const PolyOptic(this.run);
+  const PolyOptic._(this.run);
+
+  static PolyOptic<Whole, TWhole, Part, TPart> fromRun<Whole, TWhole, Part, TPart>(Func<Func<Part, TPart>, Func<Whole, TWhole>> run) {
+    return PolyOptic._(run);
+  }
 
   static PolyOptic<T, T, T, T> id<T>() {
-    return PolyOptic(idfunc);
+    return PolyOptic.fromRun(idfunc);
   }
 
   static PolyOptic<Whole, TWhole, Part, TPart> iso<Whole, TWhole, Part, TPart>(
     Func<Whole, Part> focus,
     Func<TPart, TWhole> reconstruct,
   ) {
-    return PolyOptic((update) {
+    return PolyOptic.fromRun((update) {
       return (whole) {
         final part = focus(whole);
         final updated = update(part);
@@ -34,7 +42,7 @@ final class PolyOptic<Whole, TWhole, Part, TPart> {
     Func<Whole, Part> focus,
     Func<Whole, Func<TPart, TWhole>> reconstruct,
   ) {
-    return PolyOptic((update) {
+    return PolyOptic.fromRun((update) {
       return (whole) {
         final part = focus(whole);
         final updated = update(part);
@@ -47,7 +55,7 @@ final class PolyOptic<Whole, TWhole, Part, TPart> {
     Func<Whole, Either<TWhole, Part>> focus,
     Func<TPart, TWhole> reconstruct,
   ) {
-    return PolyOptic((update) {
+    return PolyOptic.fromRun((update) {
       return (whole) {
         final partOrNewWhole = focus(whole);
         final updatedOrNewWhole = partOrNewWhole.rmap(update);
@@ -56,30 +64,64 @@ final class PolyOptic<Whole, TWhole, Part, TPart> {
     });
   }
 
-  static PolyOptic<Whole, TWhole, Part, TPart> affine<Whole, TWhole, Part, TPart>(
-    Func<Whole, Either<TWhole, Part>> focus,
-    Func<Whole, Func<TPart, TWhole>> reconstruct,
-  ) {
-    return PolyOptic((update) {
-      return (whole) {
-        final partOrNewWhole = focus(whole);
-        final updatedOrNewWhole = partOrNewWhole.rmap(update);
-
-        return updatedOrNewWhole.rmap(reconstruct(whole)).value();
+  static PolyOptic<IList<Part>, IList<TPart>, Part, TPart> list<Part, TPart>() {
+    return PolyOptic.fromRun((update) {
+      return (functor) {
+        return functor.rmap(update);
       };
     });
   }
 
-  static PolyOptic<IList<Part>, IList<TPart>, Part, TPart> each<Part, TPart>() {
-    return PolyOptic((update) {
-      return (list) {
-        return list.rmap(update);
+  static PolyOptic<Option<Part>, Option<TPart>, Part, TPart> option<Part, TPart>() {
+    return PolyOptic.fromRun((update) {
+      return (functor) {
+        return functor.rmap(update);
+      };
+    });
+  }
+
+  static PolyOptic<Writer<E, Part>, Writer<E, TPart>, Part, TPart> writer<E, Part, TPart>() {
+    return PolyOptic.fromRun((update) {
+      return (functor) {
+        return functor.rmap(update);
+      };
+    });
+  }
+
+  static PolyOptic<Either<E, Part>, Either<E, TPart>, Part, TPart> either<E, Part, TPart>() {
+    return PolyOptic.fromRun((update) {
+      return (functor) {
+        return functor.rmap(update);
+      };
+    });
+  }
+
+  static PolyOptic<These<E, Part>, These<E, TPart>, Part, TPart> these<E, Part, TPart>() {
+    return PolyOptic.fromRun((update) {
+      return (functor) {
+        return functor.rmap(update);
+      };
+    });
+  }
+
+  static PolyOptic<(E, Part), (E, TPart), Part, TPart> tuple<E, Part, TPart>() {
+    return PolyOptic.fromRun((update) {
+      return (functor) {
+        return functor.rmap(update);
+      };
+    });
+  }
+
+  static PolyOptic<Validator<E, Part>, Validator<E, TPart>, Part, TPart> validator<E, Part, TPart>() {
+    return PolyOptic.fromRun((update) {
+      return (functor) {
+        return functor.rmap(update);
       };
     });
   }
 
   PolyOptic<Whole, TWhole, Sub, TSub> then<Sub, TSub>(PolyOptic<Part, TPart, Sub, TSub> other) {
-    return PolyOptic((update) {
+    return PolyOptic.fromRun((update) {
       return (whole) {
         return run((part) {
           return other.run(update)(part);
@@ -90,11 +132,5 @@ final class PolyOptic<Whole, TWhole, Part, TPart> {
 
   PolyOptic<Whole2, TWhole2, Part, TPart> after<Whole2, TWhole2>(PolyOptic<Whole2, TWhole2, Whole, TWhole> other) {
     return other.then(this);
-  }
-}
-
-extension PolyOpticToOpticExtension<Whole, Part> on PolyOptic<Whole, Whole, Part, Part> {
-  Optic<Whole, Part> asOptic() {
-    return Optic(run);
   }
 }
