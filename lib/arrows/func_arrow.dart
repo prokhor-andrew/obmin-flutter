9018e254a5540d2f865e3b4a67373d41758199b8
andriiprokhorenko <prokhor.andrew@gmail.com>
2025-09-26 19:58:04 -0500

    renamed get_arrow into func_arrow

--- lib/arrows/func_arrow.dart
+++ lib/arrows/func_arrow.dart
@@ -9,59 +9,59 @@ import 'package:obmin/arrows/option_arrow.dart';
 import 'package:obmin/types/func.dart';
 import 'package:obmin/types/option.dart';
 
-final class GetArrow<Whole, Part> {
+final class FuncArrow<Whole, Part> {
   final Func<Whole, Part> run;
 
-  const GetArrow._(this.run);
+  const FuncArrow._(this.run);
 
-  static GetArrow<Whole, Part> fromRun<Whole, Part>(Func<Whole, Part> run) {
-    return GetArrow._(run);
+  static FuncArrow<Whole, Part> fromRun<Whole, Part>(Func<Whole, Part> run) {
+    return FuncArrow._(run);
   }
 
-  static GetArrow<Whole, ()> unit<Whole>() {
-    return GetArrow.fromRun(constfunc(()));
+  static FuncArrow<Whole, ()> unit<Whole>() {
+    return FuncArrow.fromRun(constfunc(()));
   }
 
-  static GetArrow<A, A> id<A>() {
-    return GetArrow.fromRun(idfunc);
+  static FuncArrow<A, A> id<A>() {
+    return FuncArrow.fromRun(idfunc);
   }
 
-  GetArrow<Whole, Part2> rmap<Part2>(Func<Part, Part2> f) {
-    return GetArrow.fromRun((whole) {
+  FuncArrow<Whole, Part2> rmap<Part2>(Func<Part, Part2> f) {
+    return FuncArrow.fromRun((whole) {
       return f(run(whole));
     });
   }
 
-  GetArrow<Whole2, Part> cmap<Whole2>(Func<Whole2, Whole> f) {
-    return GetArrow.fromRun((whole2) {
+  FuncArrow<Whole2, Part> cmap<Whole2>(Func<Whole2, Whole> f) {
+    return FuncArrow.fromRun((whole2) {
       return run(f(whole2));
     });
   }
 
-  GetArrow<Whole2, Part2> promap<Whole2, Part2>(Func<Whole2, Whole> lf, Func<Part, Part2> rf) {
+  FuncArrow<Whole2, Part2> promap<Whole2, Part2>(Func<Whole2, Whole> lf, Func<Part, Part2> rf) {
     return cmap(lf).rmap(rf);
   }
 
-  GetArrow<Whole, Sub> then<Sub>(GetArrow<Part, Sub> other) {
-    return GetArrow.fromRun((whole) {
+  FuncArrow<Whole, Sub> then<Sub>(FuncArrow<Part, Sub> other) {
+    return FuncArrow.fromRun((whole) {
       return other.run(run(whole));
     });
   }
 
-  GetArrow<Whole2, Part> after<Whole2>(GetArrow<Whole2, Whole> other) {
+  FuncArrow<Whole2, Part> after<Whole2>(FuncArrow<Whole2, Whole> other) {
     return other.then(this);
   }
 
-  GetArrow<Whole, (Part, Part2)> zip<Part2>(GetArrow<Whole, Part2> other) {
-    return GetArrow.fromRun((whole) {
+  FuncArrow<Whole, (Part, Part2)> zip<Part2>(FuncArrow<Whole, Part2> other) {
+    return FuncArrow.fromRun((whole) {
       final part = run(whole);
       final part2 = other.run(whole);
       return (part, part2);
     });
   }
 
-  static GetArrow<Whole, IList<Part>> zipAll<Whole, Part>(IList<GetArrow<Whole, Part>> list) {
-    return list.fold(GetArrow.id(), (current, element) {
+  static FuncArrow<Whole, IList<Part>> zipAll<Whole, Part>(IList<FuncArrow<Whole, Part>> list) {
+    return list.fold(FuncArrow.id(), (current, element) {
       final arrow = element.rmap((value) => [value].lock);
       return current.zip(arrow).rmap((tuple) => tuple.$1.addAll(tuple.$2));
     });
