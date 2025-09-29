8ef9ece27fa7da6e178b2b1d93a16514e0878459
andriiprokhorenko <prokhor.andrew@gmail.com>
2025-09-29 15:05:56 -0500

    added explicit typing for writer type

--- lib/types/writer.dart
+++ lib/types/writer.dart
@@ -15,20 +15,20 @@ final class Writer<A, B> {
 
   const Writer(this._list, this._value);
 
-  static Writer<A, B> of<A, B>(B value) => Writer(const IList.empty(), value);
+  static Writer<A, B> of<A, B>(B value) => Writer<A, B>(IList<A>.empty(), value);
 
-  static Writer<A, ()> unit<A>() => Writer.of(());
+  static Writer<A, ()> unit<A>() => Writer.of<A, ()>(());
 
   Writer<A, T2> rmap<T2>(Func<B, T2> f) {
     return Writer(_list, f(_value));
   }
 
   Writer<C2, B> lmap<C2>(Func<A, C2> f) {
-    return Writer(_list.map(f).toIList(), _value);
+    return Writer(_list.map<C2>(f).toIList(), _value);
   }
 
   Writer<A2, B2> bimap<A2, B2>(Func<A, A2> lf, Func<B, B2> rf) {
-    return lmap(lf).rmap(rf);
+    return lmap<A2>(lf).rmap<B2>(rf);
   }
 
   Writer<A, T2> bind<T2>(Func<B, Writer<A, T2>> f) {
@@ -42,9 +42,9 @@ final class Writer<A, B> {
   }
 
   static Writer<E, IList<Part>> zipAll<E, Part>(IList<Writer<E, Part>> list) {
-    return list.fold(Writer.of(const IList.empty()), (current, element) {
-      final writer = element.rmap((value) => [value].lock);
-      return current.zip(writer).rmap((tuple) => tuple.$1.addAll(tuple.$2));
+    return list.fold<Writer<E, IList<Part>>>(Writer.of<E, IList<Part>>(IList<Part>.empty()), (current, element) {
+      final writer = element.rmap<IList<Part>>((value) => [value].lock);
+      return current.zip<IList<Part>>(writer).rmap<IList<Part>>((tuple) => tuple.$1.addAll(tuple.$2));
     });
   }
 
@@ -71,6 +71,6 @@ final class Writer<A, B> {
 
 extension WriterMonadExtension<E, T> on Writer<E, Writer<E, T>> {
   Writer<E, T> joined() {
-    return bind(idfunc);
+    return bind<T>(idfunc<Writer<E, T>>);
   }
 }
