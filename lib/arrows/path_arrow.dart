24b8ac6434f4caacbecbf0a2dfe2dfd31fd0964c
andriiprokhorenko <prokhor.andrew@gmail.com>
2025-10-15 15:18:06 -0500

    removed const IMap.empty

--- lib/arrows/path_arrow.dart
+++ lib/arrows/path_arrow.dart
@@ -291,7 +291,7 @@ final class PathArrow<Whole, Part> {
 
   PathArrow<Whole, (Part, Part2)> zipPointIndex<Part2>(PathArrow<Whole, Part2> other) {
     return PathArrow.fromRun((whole) {
-      IMap<IList<String>, (Part, Part2)> out = const IMap<IList<String>, (Part, Part2)>.empty();
+      IMap<IList<String>, (Part, Part2)> out = IMap<IList<String>, (Part, Part2)>.empty();
       final map1 = run(whole);
       final map2 = other.run(whole);
 
@@ -309,7 +309,7 @@ final class PathArrow<Whole, Part> {
 
   PathArrow<Whole, (Part, Part2)> zipCrossJoin<Part2>(PathArrow<Whole, Part2> other) {
     return PathArrow.fromRun<Whole, (Part, Part2)>((whole) {
-      IMap<IList<String>, (Part, Part2)> out = const IMap<IList<String>, (Part, Part2)>.empty();
+      IMap<IList<String>, (Part, Part2)> out = IMap<IList<String>, (Part, Part2)>.empty();
       for (final w1 in run(whole).entries) {
         final (log1, p1) = (w1.key, w1.value);
         for (final w2 in other.run(whole).entries) {
